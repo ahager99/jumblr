15e0d66d20ae240f9cef8ca1db91c17cf15a4c54
q349424 <andreas.hager@bmw.de>
2019-11-20 20:23:25 +0100

    Added souce and filename attributes

--- src/main/java/com/tumblr/jumblr/types/PhotoSize.java
+++ src/main/java/com/tumblr/jumblr/types/PhotoSize.java
@@ -1,13 +1,20 @@
 package com.tumblr.jumblr.types;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FilenameUtils;
+
 /**
  * This class represents a photo at a given size
  * @author jc
  */
-public class PhotoSize {
+public class PhotoSize extends SourceInterface {
 
     private int width, height;
     private String url;
+    private String filename;
 
     /**
      * Get the URL of this photo at this size
@@ -31,6 +38,25 @@ public class PhotoSize {
      */
     public int getHeight() {
         return height;
+    }
+
+    @Override
+    public String getSrc() {
+         return url;
+    }
+
+    @Override
+    public String getFileName() {
+        if (filename == null) {
+            URL urlObj;
+            try {
+                urlObj = new URL(url);
+                filename = FilenameUtils.getName(urlObj.getPath());
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(PhotoSize.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return filename;
     }
 
 }
--- src/main/java/com/tumblr/jumblr/types/Video.java
+++ src/main/java/com/tumblr/jumblr/types/Video.java
@@ -1,13 +1,24 @@
 package com.tumblr.jumblr.types;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FilenameUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * An individual Video in a VideoPost
  * @author jc
  */
-public class Video {
+public class Video extends SourceInterface {
 
     private Integer width;
     private String embed_code;
+    String src;
+    String type;
+    String filename;
 
     /**
      * Get the width of this video
@@ -23,6 +34,51 @@ public class Video {
      */
     public String getEmbedCode() {
         return embed_code;
+    }
+
+    
+    private void getSourceData() {
+        if (getEmbedCode() != null) {
+            Document doc = Jsoup.parse(getEmbedCode());
+            URL urlObj;
+            try {
+                src = doc.getElementsByAttribute("src").first().attr("src");
+                type =  doc.getElementsByAttribute("type").first().attr("type");
+
+                urlObj = new URL(src);
+                filename = FilenameUtils.getName(urlObj.getPath());
+                // If not contains tumblr then it's not a correct filename
+                if (!filename.contains("tumblr")) {
+                    String tmpSrc = src.substring(0, src.lastIndexOf("/"));
+                    urlObj = new URL(tmpSrc);
+                    filename = FilenameUtils.getName(urlObj.getPath());
+                }
+                filename += "." + type.substring(type.lastIndexOf("/") + 1);
+                
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(PhotoSize.class.getName()).log(Level.SEVERE, null, ex);
+                filename = null;
+            } catch (NullPointerException ex) {
+                Logger.getLogger(PhotoSize.class.getName()).log(Level.SEVERE, null, ex);
+                filename = null;
+            }
+        }
+    }
+    
+    @Override
+    public String getSrc() {
+        if (filename == null) {
+            getSourceData();
+        }
+        return "https://vtt.tumblr.com/" + filename;
+    }
+
+    @Override
+    public String getFileName() {
+        if (src == null) {
+            getSourceData();
+        }
+        return filename;
     }
 
 }
